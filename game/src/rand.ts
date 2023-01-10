a5d689a003b10813f689a56041472fc30db13513
William Henderson <william-henderson@outlook.com>
2023-01-10 21:24:52 +0000

    fix: floating point error in LCG calculations

--- game/src/rand.ts
+++ game/src/rand.ts
@@ -1,18 +1,18 @@
 class Random {
-  private seed: number;
-  private modulus: number = Math.pow(2, 31);
-  private multiplier: number = 1103515245;
-  private increment: number = 12345;
+  private seed: bigint;
+  private modulus: bigint = 2147483648n;
+  private multiplier: bigint = 1103515245n;
+  private increment: bigint = 12345n;
 
   private precision: number = 1000;
 
-  constructor(seed?: number) {
-    this.seed = seed || new Date().getTime();
+  constructor(seed?: bigint | number) {
+    this.seed = BigInt(seed || new Date().getTime());
   }
 
-  public next(seed: number = this.seed): number {
+  public next(): number {
     this.seed = (this.multiplier * this.seed + this.increment) % this.modulus;
-    return this.seed;
+    return Number(this.seed);
   }
 
   public random(): number {
